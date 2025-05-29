2348b796faf68edd476348a9e0cea01bee492e1e
Carmen Ortega <carmenortegarojas@gmail.com>
2025-05-29 17:45:11 -0500

    feat: configurar Keycloak para autenticación y enrutamiento de actualizaciones con acceso basado en roles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideKeycloak } from 'keycloak-angular';
+
+import { environment } from '../app/environments/environment';
+
+import { KeycloakOnLoad } from 'keycloak-js';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-  provideHttpClient()]
+  providers: [
+    provideKeycloak({
+      config: {
+        url: environment.keycloak.config.url,
+        realm: environment.keycloak.config.realm,
+        clientId: environment.keycloak.config.clientId
+      },
+      initOptions: {
+        onLoad: environment.keycloak.initOptions.onLoad as KeycloakOnLoad,
+        checkLoginIframe: environment.keycloak.initOptions.checkLoginIframe
+      }
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home/home.component';
+import { SmartphonesComponent } from './features/smartphones/smartphones.component';
 import { NewsletterComponent } from './shared/components/newsletter/newsletter.component';
+import { canActivateAuthRole } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {
+        path: '', redirectTo: '/home', pathMatch: 'full'
+    },
+    { path: 'home', component: HomeComponent, canActivate: [canActivateAuthRole],
+        data: {
+            roles: ['admin']
+        },
+     },
+    { path: 'smartphones', component: SmartphonesComponent },
     { path: 'newsletter', component: NewsletterComponent },
     { path: '*', redirectTo: '/home' }
 ];
