d53d75c67a3ca9e02878ea60d8b8b4a373a99be2
Carmen Ortega <carmenortegarojas@gmail.com>
2025-05-28 20:49:54 -0500

    feat: update asset paths and configure routing for home and newsletter components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home/home.component';
+import { NewsletterComponent } from './shared/components/newsletter/newsletter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'newsletter', component: NewsletterComponent },
+    { path: '*', redirectTo: '/home' }
+];
