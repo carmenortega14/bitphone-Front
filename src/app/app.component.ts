014963b405d86deb329c7b00b582965d8ac0bc75
Carmen Ortega <carmenortegarojas@gmail.com>
2025-05-28 20:49:32 -0500

    feat: implement header and footer components in app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../app/shared/components/header/header.component';
+import { FooterComponent } from '../app/shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
