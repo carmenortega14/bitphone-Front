697792c12cb21c9c754f69718858dbf1e9cbaaf7
Carmen Ortega <carmenortegarojas@gmail.com>
2025-06-02 13:34:53 -0500

    simplificar la lógica de control de acceso en auth guard para soportar múltiples roles

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,6 +2,36 @@ import { AuthGuardData, createAuthGuard } from 'keycloak-angular';
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { inject } from '@angular/core';
 
+const isAccessAllowed = async (
+  route: ActivatedRouteSnapshot,
+  __: RouterStateSnapshot,
+  authData: AuthGuardData
+): Promise<boolean | UrlTree> => {
+  const { authenticated, grantedRoles } = authData;
+
+  const requiredRoles = route.data['roles'];
+  if (!requiredRoles || requiredRoles.length === 0) {
+    return true; // Si no hay roles requeridos, permitir acceso
+  }
+
+  // Obtener los roles del cliente específico (bitphone-client)
+  const clientRoles = grantedRoles.resourceRoles['bitphone-client'] || [];
+  
+  // Verificar si el usuario tiene al menos uno de los roles requeridos
+  const hasRequiredRole = requiredRoles.some((role:any) => 
+    clientRoles.includes(role)
+  );
+
+  if (authenticated && hasRequiredRole) {
+      return true;
+  }
+  
+  const router = inject(Router);
+  return router.parseUrl('/unauthorised');
+};
+
+export const canActivateAuthRole = createAuthGuard<CanActivateFn>(isAccessAllowed);
+
 /**
  * The logic below is a simple example, please make it more robust when implementing in your application.
  *
@@ -34,33 +64,3 @@ import { inject } from '@angular/core';
 // };
 
 // export const canActivateAuthRole = createAuthGuard<CanActivateFn>(isAccessAllowed);
-
-const isAccessAllowed = async (
-  route: ActivatedRouteSnapshot,
-  __: RouterStateSnapshot,
-  authData: AuthGuardData
-): Promise<boolean | UrlTree> => {
-  const { authenticated, grantedRoles } = authData;
-
-  const requiredRoles = route.data['roles']; // Cambiado a 'roles' (plural) para soportar múltiples roles
-  if (!requiredRoles || requiredRoles.length === 0) {
-    return true; // Si no hay roles requeridos, permitir acceso
-  }
-
-  // Obtener los roles del cliente específico (bitphone-client)
-  const clientRoles = grantedRoles.resourceRoles['bitphone-client'] || [];
-  
-  // Verificar si el usuario tiene al menos uno de los roles requeridos
-  const hasRequiredRole = requiredRoles.some((role:any) => 
-    clientRoles.includes(role)
-  );
-
-  if (authenticated && hasRequiredRole) {
-      return true;
-  }
-  
-  const router = inject(Router);
-  return router.parseUrl('/unauthorised');
-};
-
-export const canActivateAuthRole = createAuthGuard<CanActivateFn>(isAccessAllowed);
